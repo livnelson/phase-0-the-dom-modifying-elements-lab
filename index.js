bb130e46c2e92f9042e638781d985b6474484054
Liv Nelson <livnelson@me.com>
2022-10-13 09:46:18 -0700

    commit tree aa9621c880310d368199f9741eb45bd3f4a5f2be

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// Write your code here!
+const main = document.getElementById("main");
+// console.log(main)
+
+main.remove()
+
+const newHeader = document.createElement("h1")
+newHeader.id = "victory"
+newHeader.textContent = "Liv is the champion"
+
+const body = document.querySelector('body')
+body.append(newHeader)
+// console.log(newHeader)
